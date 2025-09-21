700f961fafe185a04adb3433c9f38634daa35737
pathummaofficial-svg <pathumma.official@gmail.com>
2025-09-21 18:16:48 +1000

    Create db_mysqli.php

---
+++ db_mysqli.php
@@ -0,0 +1,8 @@
+<?php
+// db_mysqli.php
+$mysqli = new mysqli('127.0.0.1', 'root', '', 'traveller_db'); // <- change DB name if needed
+if ($mysqli->connect_errno) {
+  http_response_code(500);
+  exit('DB connect failed: ' . $mysqli->connect_error);
+}
+$mysqli->set_charset('utf8mb4');
